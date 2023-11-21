26f7013197ded1d0c3424c7ab0c314d41e53a6f6
SofiaaaaL <yl8586@nyu.edu>
2023-11-21 01:00:22 -0500

    second commit on 11/20/2023

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
-export default function Home() {
+import Header from "@/app/components/Header";
+
+export default function UserProfile() {
     return(
-        <div>
-            <h1>Users & Authentication</h1>
-        </div>
+        //the brackets are fragment, reason to use is that to make sure at the parent level of the component, everything is wrapped in a single element
+        <>
+            <Header />
+            <main>
+                <h1>User Profile</h1>
+            </main>
+        </>
     );
 }
