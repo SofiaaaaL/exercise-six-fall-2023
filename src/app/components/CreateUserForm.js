2930a8fbf5904bd22cc8f7ffdfaf14a8704c99c1
SofiaaaaL <yl8586@nyu.edu>
2023-12-07 09:00:51 -0500

    3rd commit on 12/07

--- src/app/components/CreateUserForm.js
+++ src/app/components/CreateUserForm.js
@@ -5,7 +5,6 @@ const CreateUserForm = ({ createUser }) => {
     return(
         <div>
             <h2>Create User Form</h2>
-            {/* when the form is submitted will run this callback */}
             <form className={styles.Form} onSubmit={(e) => createUser(e)}>
                 <label htmlFor="name">Name</label>
                 <input type="text" name="name" />
--- src/app/components/LoginForm.js
+++ src/app/components/LoginForm.js
@@ -1,9 +1,6 @@
 import styles from './components.module.css'
 
 const LoginForm = ({ loginUser }) => {
-    //e represents the Element that the function called on 
-    //in this case it is the entire form. we will use this to
-    //reference the values of the inputs in the form.
     return (
         <div>
             <h2>Login Form</h2>
